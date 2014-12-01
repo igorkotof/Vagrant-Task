1660fcbf42c6117ce2bc7a4094222a371843c281
igorkotof <igor.kotof@gmail.com>
2014-12-01 10:11:55 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,4 +2,7 @@ echo "Start provisioning"
 sudo apt-get update
 sudo apt-get install openjdk-7-jre -y
 sudo apt-get install jenkins -y
+sudo apt-get install curl -y
+gpg --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3
 sudo \curl -sSL https://get.rvm.io | bash -s stable
+source /home/vagrant/.rvm/scripts/rvm
